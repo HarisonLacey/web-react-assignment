5438d3bcb3240d5e187bf83e6cca1307b905487c
Harison Lacey <harisonlacey@gmail.com>
2021-04-20 15:21:34 +0200

    new brewer form in component folder

--- web/src/components/NewBrewerForm.js
+++ web/src/components/NewBrewerForm.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { useContextWrapper } from "../cotenxtWrapper/contextWrapper";
 const axios = require("axios");
 
+// new brewer form
+
 export default function NewBrewerForm() {
   return <form>Brewer</form>;
 }
