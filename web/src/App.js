76d167715cc1e6284d33daab46f8cc018bff5125
Harison Lacey <harisonlacey@gmail.com>
2021-04-20 13:59:33 +0200

    created routes for pages in app.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Forms from "./pages/Forms";
+import Recipes from "./pages/Recipes";
+import SingleRecipe from "./pages/SingleRecipe";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/" component={Landing} exact />
+      <Route path="/new/:id" component={Forms} />
+      <Route path="/recipes" component={Recipes} />
+      <Route path="/recipes/:id" component={SingleRecipe} />
+    </Switch>
   );
 }
 
