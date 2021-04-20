435c48f57899f559ed6dd129b25eca49c01d71a8
Harison Lacey <harisonlacey@gmail.com>
2021-04-20 20:09:30 +0200

    recipe page now displays all the recipes

--- web/src/pages/Recipes.js
+++ web/src/pages/Recipes.js
@@ -1,3 +1,33 @@
+import { useState, useEffect } from "react";
+import { useContextWrapper } from "../cotenxtWrapper/contextWrapper";
+const axios = require("axios");
+
 export default function Recipes() {
-    return <div>Recipes</div>
+  const [recipes, setRecipes] = useState([]);
+  let API_HOST = useContextWrapper();
+  useEffect(() => {
+    async function fecthRecipes() {
+      try {
+        let res = await axios.get(`${API_HOST}/recipes/`, {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+        });
+        console.log(res.data);
+        setRecipes(res.data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    fecthRecipes();
+  }, [API_HOST]);
+  return (
+    <div>
+      {recipes.map((e) => (
+        <p>
+          <a href={`/recipes/item?id=${e.id}`}>{e.title}</a>
+        </p>
+      ))}
+    </div>
+  );
 }
