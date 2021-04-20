5afb600b5808ce3a66e84c6480cbd1182aa986fe
Harison Lacey <harisonlacey@gmail.com>
2021-04-20 15:22:12 +0200

    new recipe form in components folder

--- web/src/components/NewRecipeForm.js
+++ web/src/components/NewRecipeForm.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { useContextWrapper } from "../cotenxtWrapper/contextWrapper";
 const axios = require("axios");
 
+// new recipe form
+
 export default function NewRecipeForm() {
   return <form>Recipe</form>;
 }
