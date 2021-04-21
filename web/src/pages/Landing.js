95bc0db92fe0bfe96d4957029f9d793bf533e53c
Harison Lacey <harisonlacey@gmail.com>
2021-04-21 17:11:07 +0200

    linking to other pages from landing page

--- web/src/pages/Landing.js
+++ web/src/pages/Landing.js
@@ -1,8 +1,14 @@
-import { useState, useEffect } from "react";
-import React from 'react';
-import { useContextWrapper } from "../cotenxtWrapper/contextWrapper";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useContextWrapper } from "../cotenxtWrapper/contextWrapper";
 
 export default function Landing() {
-  return <div>Landings</div>;
+  return (
+    <div>
+      <a href="new/brewer"><button>Add New Brewer</button></a>
+      <a href="new/recipe"><button>Add New Recipe</button></a>
+      <a href="brewers"><button>Brewers</button></a>
+      <a href="recipes"><button>Recipes</button></a>
+    </div>
+  );
 }
