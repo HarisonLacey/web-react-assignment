2a06f272938dac0abb5a62dba5e0cba716765f65
Harison Lacey <harisonlacey@gmail.com>
2021-04-22 10:54:23 +0200

    cleanups

--- web/src/pages/Forms.js
+++ web/src/pages/Forms.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import React from 'react';
+import React from "react";
 import { useParams } from "react-router-dom";
 import NewBrewerForm from "../components/NewBrewerForm";
 import NewRecipeForm from "../components/NewRecipeForm";
--- web/src/pages/Landing.js
+++ web/src/pages/Landing.js
@@ -1,14 +1,20 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { useContextWrapper } from "../cotenxtWrapper/contextWrapper";
+import React from "react";
 
 export default function Landing() {
   return (
     <div>
-      <a href="new/brewer"><button>Add New Brewer</button></a>
-      <a href="new/recipe"><button>Add New Recipe</button></a>
-      <a href="brewers"><button>Brewers</button></a>
-      <a href="recipes"><button>Recipes</button></a>
+      <a href="new/brewer">
+        <button>Add New Brewer</button>
+      </a>
+      <a href="new/recipe">
+        <button>Add New Recipe</button>
+      </a>
+      <a href="brewers">
+        <button>Brewers</button>
+      </a>
+      <a href="recipes">
+        <button>Recipes</button>
+      </a>
     </div>
   );
 }
