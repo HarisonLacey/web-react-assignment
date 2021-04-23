24812c665bbc657c8af71880143c7a40acd0bab3
Harison Lacey <harisonlacey@gmail.com>
2021-04-23 14:02:37 +0200

    created a page to delete items from database

--- web/src/pages/Delete.js
+++ web/src/pages/Delete.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useContextWrapper } from "../contextWrapper/contextWrapper";
+import { Select } from "../styledComponents/sharedComponents";
 
 export default function DeleteItem() {
+  const [item, setItem] = useState();
   const [id, setId] = useState();
   let API_HOST = useContextWrapper();
   async function deleteItems() {
     try {
-      let res = await axios.delete(`${API_HOST}/recipes/${id}`, {
+      let res = await axios.delete(`${API_HOST}/${item}/${id}`, {
         headers: {
           "Access-Control-Allow-Origin": "*",
         },
@@ -19,7 +21,14 @@ export default function DeleteItem() {
   }
   return (
     <div>
-      <input onChange={(e) => setId(e.target.value)} />
+      <Select onChange={(e) => setItem(e.target.value)} required>
+        <option value="" disabled selected>
+          Item
+        </option>
+        <option value="brewers">Brewers</option>
+        <option value="recipes">Recipes</option>
+      </Select>
+      <input placeholder="id" onChange={(e) => setId(e.target.value)} />
       <button onClick={deleteItems}>Delete</button>
     </div>
   );
