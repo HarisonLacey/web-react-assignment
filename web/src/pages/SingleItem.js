e203351108037567a4634c76c939bd2f6cc65740
Harison Lacey <harisonlacey@gmail.com>
2021-04-24 22:14:47 +0200

    layout updates for single items

--- web/src/pages/SingleItem.js
+++ web/src/pages/SingleItem.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useContextWrapper } from "../contextWrapper/contextWrapper";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function SingleItem() {
   const [item, setItem] = useState({});
@@ -48,27 +49,50 @@ export default function SingleItem() {
     Item();
   }, [api, index, type]);
   return (
-    <div>
-      {type === "recipes" && (
-        <div>
-          <p>{item.title}</p>
-          <p>
-            <Link to={`/brewers/${item.brewer_id}`}>Visit Brewer</Link>
-          </p>
-        </div>
-      )}
-      {type === "brewers" && (
-        <div>
-          <p>{item.name}</p>
-          <ul>
-            {recipes.map((recipe) => (
-              <li>
-                <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
+    <Container style={{ padding: "50px 0 0" }} fluid>
+      <Row noGutters>
+        {type === "recipes" && (
+          <>
+            <Col sm={6} lg={4}>
+              <p>{item.title}</p>
+            </Col>
+            <Col sm={3} lg={4}></Col>
+            <Col sm={3} lg={4}></Col>
+            <Col sm={6} lg={4}>
+              info
+            </Col>
+            <Col sm={3} lg={4}></Col>
+            <Col sm={3} lg={4}></Col>
+            <Col sm={6} lg={4}>
+              <p>
+                <Link to={`/brewers/${item.brewer_id}`}>Visit Brewer</Link>
+              </p>
+            </Col>
+            <Col sm={3} lg={4}></Col>
+            <Col sm={3} lg={4}></Col>
+          </>
+        )}
+        {type === "brewers" && (
+          <>
+            <Col xs={12} sm={6} lg={4}>
+              <p>{item.name}</p>
+            </Col>
+            <Col sm={3} lg={4}></Col>
+            <Col sm={3} lg={4}></Col>
+            <Col sm={6} lg={4}>
+              <ul>
+                {recipes.map((recipe) => (
+                  <li>
+                    <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+                  </li>
+                ))}
+              </ul>
+            </Col>
+            <Col sm={3} lg={4}></Col>
+            <Col sm={3} lg={4}></Col>
+          </>
+        )}
+      </Row>
+    </Container>
   );
 }
