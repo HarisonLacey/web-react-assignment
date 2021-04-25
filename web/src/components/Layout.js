221208f7ec43d8f0e3172c8b62d1ef32f9ed2e2f
Harison Lacey <harisonlacey@gmail.com>
2021-04-25 15:45:01 +0200

    added facebook logo to footer

--- web/src/components/Layout.js
+++ web/src/components/Layout.js
@@ -1,11 +1,34 @@
 import React from "react";
+import Header from "./Header";
+import { Container, Row, Col } from "react-bootstrap";
+import facebook from "../assets/facebook.png";
 
 export default function Layout({ children }) {
   return (
     <>
-      <header>Header</header>
-      <main>{children}</main>
-      <footer>Footer</footer>
+      <header>
+        <Header />
+      </header>
+      <main style={{ paddingTop: "100px" }}>{children}</main>
+      <footer style={{ height: "100px", backgroundColor: "#314e52" }}>
+        <Container fluid>
+          <Row noGutters>
+            <Col xs={4}></Col>
+            <Col
+              xs={4}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                paddingTop: "5px",
+                height: "100px"
+              }}
+            >
+              <img style={{ margin: "0 auto" }} src={facebook} />
+            </Col>
+            <Col xs={4}></Col>
+          </Row>
+        </Container>
+      </footer>
     </>
   );
 }
