62b49ee95e83452c778132c7353d593791dc54b8
Harison Lacey <harisonlacey@gmail.com>
2021-04-25 21:22:05 +0200

    style updates to shared styled components

--- web/src/styledComponents/sharedComponents.js
+++ web/src/styledComponents/sharedComponents.js
@@ -10,9 +10,10 @@ export const Form = styled.form`
     display: block;
     width: 90%;
     margin: 2% auto;
-    font-family: 'Fredoka One', cursive;
+    font-family: "Fredoka One", cursive;
     color: #314e52;
     border: none;
+    font-size: 1.5em;
   }
   button {
     height: 50px;
@@ -20,14 +21,16 @@ export const Form = styled.form`
 `;
 
 // select component
-export const Select = styled.select.attrs(({ inline }) => ({
+export const Select = styled.select.attrs(({ inline, size }) => ({
   display: inline ? "inline-block" : "block",
   width: inline ? "30%" : "90%",
-  margin: inline ? "2%" : "2% 0",
+  margin: inline ? "1%" : "2% auto",
+  font: size || "1.5em",
 }))`
   width: ${({ width }) => width};
   margin: ${({ margin }) => margin};
-  font-family: 'Fredoka One', cursive; 
+  font-family: "Fredoka One", cursive;
+  font-size: ${({ font }) => font};
   color: #314e52;
   border: none;
   display: ${({ display }) => display};
@@ -42,27 +45,40 @@ export const Select = styled.select.attrs(({ inline }) => ({
   }
 `;
 
-export const Button = styled.button`
-border: none;
-background-color: #ffefa0;
-font-family: 'Fredoka One', cursive; 
-border-radius: 5px;
-color: #314e52;
-:hover {
-  background-color: #314e52;
-  color: whitesmoke;
-}
-`
+export const Button = styled.button.attrs(({ height }) => ({
+  height: height || "1.5em",
+}))`
+  border: none;
+  height: ${({ height }) => height};
+  background-color: #ffefa0;
+  font-family: "Fredoka One", cursive;
+  border-radius: 5px;
+  color: #314e52;
+  :hover {
+    background-color: #314e52;
+    color: whitesmoke;
+  }
+  a {
+    color: #314e52;
+  }
+  a:hover {
+    color: whitesmoke;
+  }
+`;
 export const ItemDisplay = styled.div`
-width: 90%;
-font-family: 'Monoton', cursive;
-margin: 0 auto;
-height: 300px;
-display: flex;
-align-items: center;
-background-color: #314e52;
-color: #ffefa0;
-h2 {
-margin: 0 auto;
-}
-`
+  width: 90%;
+  font-family: "Monoton", cursive;
+  margin: 0 auto;
+  height: 300px;
+  display: flex;
+  align-items: center;
+  background-color: #314e52;
+  color: #ffefa0;
+  :hover {
+    box-shadow: 5px 10px black;
+  }
+  h2 {
+    text-align: center;
+    margin: 0 auto;
+  }
+`;
