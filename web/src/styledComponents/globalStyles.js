ca42a821f50dd65c2734d98828f14943a20a2966
Harison Lacey <harisonlacey@gmail.com>
2021-04-25 21:53:46 +0200

    style updates

--- web/src/styledComponents/globalStyles.js
+++ web/src/styledComponents/globalStyles.js
@@ -4,13 +4,24 @@ import { createGlobalStyle } from "styled-components";
 
 // global style
 export const GlobalStyle = createGlobalStyle`
+body {
+  background-color: #314e52;
+  font-family: 'Lobster', cursive;
+}
+a:hover {
+  text-decoration: none;
+}
 `;
 
 // styled themes
 export const themes = {
   colors: {
-    primary: "red",
-    secondary: "blue",
+    primary: "#314e52",
+    secondary: "#ffefa0",
+    thirdly: "whitesmoke",
   },
+  fonts: {
+
+  }
 };
 
